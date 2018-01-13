0bae682bdb9ff17efbd0403f1fc5183d186f2ee8
sagar <ruparelia.sagar@gmail.com>
2018-01-13 03:31:49 -0500

    code refactored

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,19 +1,23 @@
 import { Injectable } from '@angular/core';
 import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import {AuthService} from './auth.service';
+import 'rxjs/add/operator/take';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private router: Router) { }
+  constructor(private router: Router, private authService: AuthService) { }
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    if (localStorage.getItem('currentUser')) {
-      // logged in so return true
-      return true;
-    }
-
-    // not logged in so redirect to login page with the return url
-    this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
-    return false;
+    return this.authService.isLoggedIn
+      .take(1)
+      .map((isLoggedIn: boolean) => {
+        if (!isLoggedIn) {
+          this.router.navigate(['/login']);  // {4}
+          return false;
+        }
+        return true;
+      });
   }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,22 +4,47 @@ import {User} from './user';
 import {HttpClient} from '@angular/common/http';
 import 'rxjs/add/operator/map';
 import {Router} from '@angular/router';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 
 @Injectable()
 export class AuthService {
+  private loggedIn = new BehaviorSubject<boolean>(this.checkLogin());
+
+  get isLoggedIn() {
+    console.log('loggedIn'+ this.loggedIn.getValue());
+    return this.loggedIn.asObservable(); // {2}
+  }
+
+  checkLogin(): boolean {
+    const user = window.localStorage.getItem('currentUser');
+    if (user) {
+      try {
+        if (JSON.parse(user).user_name) {
+          return true;
+        }
+      } catch (e) {
+      }
+    }
+    return false;
+  }
+
 
   constructor(private http: HttpClient, private router: Router) { }
 
   login(body: {email: string, pwd: string}, url: string) {
     return this.http.post<User>('/API/login', body)
       .map((data) => {
+        this.loggedIn.next(true);
         window.localStorage.setItem('currentUser', JSON.stringify(data));
         this.router.navigate([url]);
       });
   }
 
+
   logout() {
     window.localStorage.removeItem('currentUser');
+    this.loggedIn.next(false);
+    // this.router.navigate(['/login']);
   }
 
   getUser(): User {
